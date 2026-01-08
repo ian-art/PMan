f96d2f758df821cfa8163877517d5f91af4df739
ian-art <mohkin97@gmail.com>
2026-01-09 03:15:05 +0800

    License: migrate from MIT to GPL-3.0-or-later - All project source and header files now carry a full GNU GPL v3.0 license header. This change standardizes licensing and aligns the project with GPL-only components.

--- include/version.h
+++ include/version.h
@@ -1,8 +1,27 @@
+/*
+ * This file is part of Priority Manager (PMan).
+ *
+ * Copyright (c) 2025 Ian Anthony R. Tancinco
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef VERSION_H
 #define VERSION_H
 
 #define PMAN_VERSION_MAJOR 2
-#define PMAN_VERSION_MINOR 3
+#define PMAN_VERSION_MINOR 4
 #define PMAN_VERSION_PATCH 0
 #define PMAN_VERSION_BUILD 2026
 
